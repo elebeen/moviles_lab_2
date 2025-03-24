d1c416dd572278fc5c39b0a8a20a9615ded868dd
hermes curo <hermescurov5@gmail.com>
2025-03-24 12:47:21 -0500

    punto numero 6

--- src/Acuario/PecesAcuario.kt
+++ src/Acuario/PecesAcuario.kt
@@ -5,12 +5,12 @@ class PecesAcuario {
 }
 
 abstract class Pez(open var color: String, open var nombre: String) {
-    open val crearPez: String
+    fun imprimir() {}
         
 }
 
-class tiburon(override open var color: String, open var nombre: String) {
-
+class tiburon(override var color: String, var nombre: String) {
+    
 }
 
 class PezPayaso {
